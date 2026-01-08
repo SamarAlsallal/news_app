76a032ff25dee3469e77893304d8bd69a301eb69
Samar Alsallal <samaralsallal13@gmail.com>
2026-01-09 00:36:02 +0300

    some changes in categories view

--- lib/screens/category_news_view.dart
+++ lib/screens/category_news_view.dart
@@ -4,16 +4,18 @@ import 'package:news_app/Models/category_model.dart';
 import 'package:news_app/widgets/news_view_builder.dart';
 
 class CategoryNewsview extends StatelessWidget {
-  const CategoryNewsview({super.key , required this.category});
+  const CategoryNewsview({super.key, required this.category});
   final CategoryModel category;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: CustomScrollView(
-        slivers: [
-          Newsviewbuilder(categoryName: category.text,),
-        ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(elevation: 0),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10, top: 10, right: 10),
+        child: CustomScrollView(
+          slivers: [Newsviewbuilder(categoryName: category.text)],
+        ),
       ),
     );
   }
